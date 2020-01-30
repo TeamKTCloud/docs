e6ef88aab2975b6e1fde8ddb6a328467fc0c6ab4
ghk <ghk9204@gmail.com>
2020-01-31 01:09:28 +0900

    firebase update example

--- firebaseExample.py
+++ firebaseExample.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials
 cred = credentials.Certificate('./YOUR_firebase.json')
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://YOURPROJECT.firebaseio.com/'})
 
+# 딕셔너리 생성 후 firebase 노드에 업데이트
 dic = dict()
 dic['posco stock'] = {'title':"posco article6", 'up_down': str(probList), 'key_prob': str(lime_list), 'contents':input_text, 'links':'http://www.google.com'}
 
