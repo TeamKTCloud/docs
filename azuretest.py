377858bbc89cc87ff37e09be660de9aae5cb5135
DESKTOP-E1P2LUQ\kimsohyun <kim.159657@gmail.com>
2020-02-03 21:39:58 +0900

    azure_test

--- azuretest.py
+++ azuretest.py
@@ -1,16 +1,33 @@
+import os
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
+import hashlib
+import hmac
+import base64
+import requests
+import urllib
 
-# Tenant ID for your Azure subscription
-TENANT_ID = 'ad654bce-b5cb-4cc7-92b1-9683ec967c14'
+cred = credentials.Certificate('./example01-c1e3e-firebase-adminsdk-qi6cv-2975ba173f.json')
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://example01-c1e3e.firebaseio.com/'})
+
+root = db.reference()
+key = root.child('User').child('Azure').child('Key').get()
+
+# Tenant ID for your Azure subscription //Directory
+TENANT_ID = key['TENANT_ID']
 
 # Your service principal App ID
-CLIENT = 'a84ff698-df73-4c4f-b715-bf44519f60e7'
+CLIENT = key['CLIENT']
 
 # Your service principal password
-KEY = '09807e8d5020485d817809e07eaa047c'
+KEY = key['KEY']
 
-subscription_id = '2b7ba058-853a-4632-8d3d-31206ae17523'
+
+subscription_id = key['subscription_id']
 
 credentials = ServicePrincipalCredentials(
     client_id = CLIENT,
@@ -18,6 +35,15 @@ credentials = ServicePrincipalCredentials(
     tenant = TENANT_ID
 )
 
-client = ComputeManagementClient(credentials, subscription_id)
+compute_client = ComputeManagementClient(credentials, subscription_id)
 
+dic = dict()
 
+print('\nList VMs in resource group')
+for vm in compute_client.virtual_machines.list_all():
+    print("\tVM: {}".format(vm.name))
+name = format(vm.name)
+
+dic[name] = {'State':'Running'}
+
+root.child('User').child('Azure').child('Resources').child('VM').update(dic)
