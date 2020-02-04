677a6d3f9a80b3e0e7a436c4f70d311c6392efd5
DESKTOP-E1P2LUQ\kimsohyun <kim.159657@gmail.com>
2020-02-04 15:15:07 +0900

    KT+AWS+Azure

--- example01.py
+++ example01.py
@@ -1,3 +1,9 @@
+import os
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
+import boto3
+import botocore
 import firebase_admin
 from firebase_admin import db
 from firebase_admin import credentials
@@ -6,63 +12,163 @@ import hmac
 import base64
 import requests
 import urllib
+import sys
 
 
 cred = credentials.Certificate('./example01-c1e3e-firebase-adminsdk-qi6cv-2975ba173f.json')
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://example01-c1e3e.firebaseio.com/'})
 
-endPoint='https://api.ucloudbiz.olleh.com/server/v1/client/api?'
-
+VM_endPoint='https://api.ucloudbiz.olleh.com/server/v1/client/api?'
+LB_endPoint = 'https://api.ucloudbiz.olleh.com/loadbalancer/v1/client/api?'
 root = db.reference()
-key = root.child('User').child('KT').child('Key').get()
-
-params={}
-params['command']='listVirtualMachines'
-params['state']='Running'
-params['response']='json'
-params['apiKey'] = key['API_KEY']    
-secretkey = key['SECRET_KEY']  
-secretkey = secretkey.encode('utf-8')
-
-requestParams = '&'.join(['='.join([k,urllib.parse.quote_plus(params[k])]) for k in params.keys()])
-
-# signature 생성
-message = '&'.join(['='.join([k.lower(),urllib.parse.quote_plus(params[k]).lower()]) for k in sorted(params.keys())])
-message = message.encode('utf-8')
-digest = hmac.new(secretkey, msg=message, digestmod=hashlib.sha1).digest()
-signature = base64.b64encode(digest)
-signature = urllib.parse.quote_plus(signature)
-
-# Request URL 생성
-requestURL=endPoint+requestParams+'&signature='+signature
-
-# print(requestURL)
-
-# data 파싱
-data = requests.get(requestURL).json()
-# print(data)
-
-# 예제 - 현재 Zone 에서 Running 상태인 vm의 수
-# print(data['listvirtualmachinesresponse']['count'])
-# print(data['listvirtualmachinesresponse']['virtualmachine'][0]['created'])
-
-count = data['listvirtualmachinesresponse']['count']
-
-dic = dict()
-
-for i in range(count):
-    name = data['listvirtualmachinesresponse']['virtualmachine'][i]['displayname']
-    state = data['listvirtualmachinesresponse']['virtualmachine'][i]['state']
-    created = data['listvirtualmachinesresponse']['virtualmachine'][i]['created']
-    cupspeed = data['listvirtualmachinesresponse']['virtualmachine'][i]['cpuspeed']
-    dic[name] = {'State':state, 'Created':created, 'CpuSpeed':cupspeed}
-    # root = db.reference()   
-    root.child('User').child('KT').child('Resources').child('VM').update(dic)
 
 
+#KT
+class KT_instance():
+    key = root.child('User').child('KT').child('Key').get()
+    requestURL = 0
+    data = 0
+    command_list = 0
+    command = 0
+    requestParams = 0
+    
+    def CreateURL(self, command):
+        self.command = command
+        params={}
+        params['command']=self.command
+        params['response']='json'
+        params['apiKey'] = self.key['API_KEY']    
+        secretkey = self.key['SECRET_KEY']  
+        secretkey = secretkey.encode('utf-8')
+        self.requestParams = '&'.join(['='.join([k,urllib.parse.quote_plus(params[k])]) for k in params.keys()])
 
-# mnjv4D2hZAnMgik0Lej36V8qDiOca_zV78mCqHpD5hUqxzDoxPkI2xHF8ifL4YrqGWIJUuqAr2MC-cMLXRW8fw
-# w32U6OFJlmsPmfLgCdlIhfrXcM_2ApUoz2KkLwoKTTYLdI9DO4E2A4y-MB73_P0uUc42F08osJMcDyNWStwvRQ
+        # signature 생성
+        message = '&'.join(['='.join([k.lower(),urllib.parse.quote_plus(params[k]).lower()]) for k in sorted(params.keys())])
+        message = message.encode('utf-8')
+        digest = hmac.new(secretkey, msg=message, digestmod=hashlib.sha1).digest()
+        signature = base64.b64encode(digest)
+        signature = urllib.parse.quote_plus(signature)
 
-# https://api.ucloudbiz.olleh.com/server/v1/client/api?apikey=mnjv4D2hZAnMgik0Lej36V8qDiOca_zV78mCqHpD5hUqxzDoxPkI2xHF8ifL4YrqGWIJUuqAr2MC-cMLXRW8fw&command=listVirtualMachines&name&state=running&response=json&signature=BYm8YZUQ%2FzyRlV4nT2x1yW5Alcs%3D
-# https://api.ucloudbiz.olleh.com/server/v1/client/api?apikey=mnjv4D2hZAnMgik0Lej36V8qDiOca_zV78mCqHpD5hUqxzDoxPkI2xHF8ifL4YrqGWIJUuqAr2MC-cMLXRW8fw&command=listVirtualMachines&name&state=running&response=json&signature=BYm8YZUQ%2FzyRlV4nT2x1yW5Alcs%3D
+        # Request URL 생성
+        if self.command == 'listVirtualMachines':
+            self.requestURL=VM_endPoint+self.requestParams+'&signature='+signature
+            # print(self.requestURL)
+        if self.command == 'listLoadBalancers':
+            self.requestURL=LB_endPoint+self.requestParams+'&signature='+signature
+            # print(self.requestURL)
+
+    def DataParsing(self):
+        # data 파싱
+        self.data = requests.get(self.requestURL).json()
+        # print(self.data)
+
+    def DataPut(self):
+        dic = dict()
+        if self.command == 'listVirtualMachines':
+            count = self.data['listvirtualmachinesresponse']['count']
+            for i in range(count):
+                name = self.data['listvirtualmachinesresponse']['virtualmachine'][i]['displayname']
+                state = self.data['listvirtualmachinesresponse']['virtualmachine'][i]['state']
+                created = self.data['listvirtualmachinesresponse']['virtualmachine'][i]['created']
+                cupspeed = self.data['listvirtualmachinesresponse']['virtualmachine'][i]['cpuspeed']
+                dic[name] = {'State':state, 'Created':created, 'CpuSpeed':cupspeed}
+                root.child('User').child('KT').child('Resources').child('VM').update(dic)
+
+        if self.command == 'listLoadBalancers':
+            count = self.data['listloadbalancersresponse']['count']
+            for i in range(count):
+                name = self.data['listloadbalancersresponse']['loadbalancer'][i]['name']
+                id = self.data['listloadbalancersresponse']['loadbalancer'][i]['loadbalancerid']
+                state = self.data['listloadbalancersresponse']['loadbalancer'][i]['state']
+                dic[name] = {'ID':id, 'State':state}
+                root.child('User').child('KT').child('Resources').child('LB').update(dic)
+
+
+#AWS
+class AWS_instance():
+    key = root.child('User').child('AWS').child('Key').get()
+    ec2_client = 0 
+    ec2_resource = 0
+    response = 0
+
+
+    def CreateKey(self):
+        self.ec2_client = boto3.client(
+            'ec2',
+            # Hard coded strings as credentials, not recommended.
+        aws_access_key_id = self.key['aws_access_key_id'],
+        aws_secret_access_key = self.key['aws_secret_access_key'],
+        region_name = self.key['region_name']
+        )
+
+        self.ec2_resource = boto3.resource(
+            'ec2',
+            # Hard coded strings as credentials, not recommended.
+            aws_access_key_id = self.key['aws_access_key_id'],
+            aws_secret_access_key = self.key['aws_secret_access_key'],
+            region_name = self.key['region_name']
+        )
+
+    def DataPut(self):
+        dic = dict()
+        self.response = self.ec2_client.describe_instances()
+        i = 0
+        while i < len(self.response):
+            name = self.response['Reservations'][i]['Instances'][0]['Tags'][0]['Value']
+            isinstance_id = self.response['Reservations'][i]['Instances'][0]['InstanceId']
+            state = self.response['Reservations'][i]['Instances'][0]['State']['Name']
+            print(name, state, isinstance_id)
+            dic[name] = {'State' : state}
+            root.child('User').child('AWS').child('Resource').child('VM').update(dic)
+            i = i+1
+
+
+#Azure
+class Azure_instance():
+    key = root.child('User').child('Azure').child('Key').get()
+    compute_client = 0
+
+    def CreateKey(self):
+        TENANT_ID = self.key['TENANT_ID']
+        CLIENT = self.key['CLIENT']
+        KEY = self.key['KEY']
+        subscription_id = self.key['subscription_id']
+        credentials = ServicePrincipalCredentials(
+            client_id = CLIENT,
+            secret = KEY,
+            tenant = TENANT_ID
+            )
+        self.compute_client = ComputeManagementClient(credentials, subscription_id)
+
+    
+    def DataPut(self):
+        dic = dict()
+        for vm in self.compute_client.virtual_machines.list_all():
+            print("\tVM: {}".format(vm.name))
+        name = format(vm.name)
+        dic[name] = {'State':'Running'}
+        root.child('User').child('Azure').child('Resources').child('VM').update(dic)
+
+
+# #'listVirtualMachines', 'listLoadBalancers', 'usageLoadBalancerService'
+KT_instance = KT_instance()
+
+# #listVirtualMachines
+KT_instance.CreateURL('listVirtualMachines')
+KT_instance.DataParsing()
+KT_instance.DataPut()
+
+# #listLoadBalancers
+KT_instance.CreateURL('listLoadBalancers')
+KT_instance.DataParsing()
+KT_instance.DataPut()
+
+AWS_instance = AWS_instance()
+AWS_instance.CreateKey()
+AWS_instance.DataPut()
+
+Azure_instance = Azure_instance()
+Azure_instance.CreateKey()
+Azure_instance.DataPut()
+
+
